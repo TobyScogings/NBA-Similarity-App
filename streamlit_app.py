a8d253b7e17dea32a37d1e11fd0d5f0f6b3e9a51
TobyScogings <toby.scogings@bath.edu>
2025-02-17 17:24:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,10 +245,10 @@ def player_inputs(df):
             players_in_year.append(player_display_name)
             player_name_map[player_display_name] = row['Full Name']
 
-        selected_player = player_name_map[selected_display_name]
     
         if players_in_year:
             selected_display_name = st.selectbox("Select Player", players_in_year)
+            selected_player = player_name_map[selected_display_name]
     
             try:
                 index_input = df[(df['Full Name'] == selected_player) & (df['year'] == selected_year)].index[0]
